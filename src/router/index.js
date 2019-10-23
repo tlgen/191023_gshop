f50b5316525215fe1544de3ce8f1b052dd86406e
tlgen <582532183@qq.com>
2019-10-23 21:47:38 +0800

    init gshop

---
+++ src/router/index.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import MSite from '../pages/MSite/MSite'
+import Order from '../pages/Order/Order'
+import Search from '../pages/Search/Search'
+import Profile from '../pages/Profile/Profile'
+import Login from '../pages/Login/Login'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/msite',
+      component: MSite,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/order',
+      component: Order,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/search',
+      component: Search,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/profile',
+      component: Profile,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/',
+      redirect: '/msite'
+    },
+    {
+      path: '/login',
+      component: Login
+    }
+  ]
+})
